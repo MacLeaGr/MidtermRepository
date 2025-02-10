405602af1eb0015c8670accfe2164f44f343b279
MacLeaGr <macleagr@sheridancollege.ca>
2025-02-10 18:16:53 -0500

    Midterm Commit - Graham MacLean 991770586

--- src/status/StausUser.java
+++ src/status/StausUser.java
@@ -11,24 +11,9 @@ package status;
  */
 public class StausUser 
 {
-   public void statusDetail(String code)
+   public void statusDetail(int code)
 {
-switch(code.toUpperCase())
-        {
-        case "ZERO": System.out.println("REJECTED");
-        break;
-        case "ONE": System.out.println("PENDING");
-        break;
-        case "TWO":
-        System.out.println("PROCESSING");
-        break;
-        case "THREE": 
-            System.out.println("APPROVED");
-        break;
-        default:
-         System.out.println("NOT VALID CODE");
-        break;
-        }
+    System.out.println("User Status: " + Status.userOptions.values()[code]);
 }
  
 }
